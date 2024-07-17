528527df2cfd5379ecfea928dc4f739653abb228
Fei <78798119+SilviaEera@users.noreply.github.com>
2024-07-18 00:12:38 +0600

    day 4 update

--- Day 4/StaticVariable.java
+++ Day 4/StaticVariable.java
@@ -11,11 +11,18 @@ class TestStatic {
 
 public class StaticVariable {
     public static void main(String[] args) {
+
+        // if i change the static variables value in an instance, all the instance
+        // cerate
         TestStatic s1 = new TestStatic();
         s1.color = "blue";
         s1.price = 100;
 
         s1.printDetails();
+        System.out.println("===========");
+        TestStatic s2 = new TestStatic();
+
+        s2.printDetails();
 
     }
 }
