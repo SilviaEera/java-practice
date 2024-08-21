b043c7a27eebf4968db743a6c7fd4704852bd044
Fei <78798119+SilviaEera@users.noreply.github.com>
2024-08-21 23:59:47 +0600

    index II : objectvill

---
+++ OOP/OOP/src/oop/headfirst/objectvill/Airplane.java
@@ -0,0 +1,58 @@
+package oop.headfirst.objectvill;
+
+public class Airplane {
+
+	private int speed;
+	private int crewMember;
+	private String planeName;
+	private int ticketPrice;
+	
+	
+	{
+		speed = 200;
+		planeName = "Boing 357";
+		crewMember = 30;
+	}
+	
+	public Airplane(int speed, int ticketPrice) {
+		this.speed = speed;
+		this.ticketPrice = ticketPrice;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	public int getCrewMember() {
+		return crewMember;
+	}
+	
+	public String getPlaneName() {
+		return planeName;
+	}
+	
+	public int getTicketSpeed() {
+		return ticketPrice;
+	}
+	
+	public void setTicketPrice(int ticketPrice) {
+		this.ticketPrice = ticketPrice;
+	}
+	
+	public String toString() {
+		String r = "[ 'Plane name' : " + planeName + "' Speed' : " + speed + "km/h" + " 'Crew Member' : " + crewMember + " 'Ticket price' : " + ticketPrice + "BDT" + " ]" ;
+		return r;
+	}
+	
+	
+	public static void main(String [] args) {
+		Airplane ar = new Airplane(300, 70000);
+		
+		System.out.print(ar);
+	}
+	
+}
